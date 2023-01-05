8a769f7f2c8960fee31c9c8870e4281352f129bc
Renata Costa <renata_rayanne_@hotmail.com>
2023-01-05 07:57:24 -0300

    feat/ create context login and register

---
+++ src/Components/Contexts/UserContext/types.ts
@@ -0,0 +1,46 @@
+import React from "react";
+
+export interface iFormLogin {
+  email: string;
+  password: string;
+}
+
+export interface iFormRegister {
+  name: string;
+  email: string;
+  password: string;
+  // confirmPassword: StringSchema;
+  businessName: string;
+  cnpj: string;
+  telephone: string;
+  address: string;
+  profileImgUrl: string;
+}
+
+export interface iUserProviderProps {
+  children: React.ReactNode;
+}
+
+export interface iUserProviderValue {
+  user: iUser | null;
+  setUser: (user: iUser | null) => void;
+
+  loading: boolean;
+
+  userLogin: (data: iFormLogin) => Promise<void>;
+  userRegister: (data: iFormRegister) => Promise<void>;
+}
+
+export interface iUser {
+  id: string;
+  name?: string;
+  email?: string;
+  profileUrl?: string;
+  phoneNumber?: string;
+}
+
+export interface iUserResponse {
+  accessToken: string;
+
+  user: iUser;
+}
