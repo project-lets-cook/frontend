99723db7090f45b1ac25355a535e2d98e4d31418
natabgaf <natanfernandes52@gmail.com>
2023-01-06 12:23:25 -0300

    feat/Protect routes Testing

--- src/Components/ProtectedRoutes/index.tsx
+++ src/Components/ProtectedRoutes/index.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 export const ProtectedRoutes = () => {
   const { user, loadingUser } = useContext(UserContext);
