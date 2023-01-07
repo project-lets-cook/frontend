2894f2addf6b50e7906846dc642a5d2d9f3e28b8
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2023-01-06 21:41:10 -0300

    feat: add about links

--- src/Components/Carousel/index.tsx
+++ src/Components/Carousel/index.tsx
@@ -17,14 +17,15 @@ const team = [
   {
     name: "Davi S Santana",
     avatar: "https://ca.slack-edge.com/TQZR39SET-U03PBPR977H-4b241aa4920f-512",
-    linkedin: "",
-    github: "",
+    linkedin: "https://www.linkedin.com/in/davi-s-s/",
+    github: "https://github.com/DaviSouzaS",
   },
   {
     name: "Lilian Fernandes",
     avatar: "https://ca.slack-edge.com/TQZR39SET-U03QUFWL55E-422d74c631a4-512",
-    linkedin: "",
-    github: "",
+    linkedin:
+      "https://www.linkedin.com/in/lilian-fernandes-oliveira-dias-4a159578/",
+    github: "https://github.com/lilianfdias",
   },
   {
     name: "Lucas Rodrigues",
@@ -47,8 +48,8 @@ const team = [
   {
     name: "Tamir S. Ferreira",
     avatar: "https://ca.slack-edge.com/TQZR39SET-U03MXQXUQLC-34319a016b9b-512",
-    linkedin: "",
-    github: "",
+    linkedin: "https://www.linkedin.com/in/tamir-ferreira/",
+    github: "https://github.com/tamir-ferreira",
   },
 ];
 
