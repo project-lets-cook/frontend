6aca5e302e885a5417a0479988f794f2ac4733d4
Lucas Rodrigues <lcs.rs23@gmail.com>
2023-01-09 17:05:27 -0300

    fix: select background

--- src/Components/Forms/Select/styled.ts
+++ src/Components/Forms/Select/styled.ts
@@ -4,22 +4,23 @@ export const SelectCityStyled = styled.form`
     width: 250px;
     margin-bottom: 20px;
     position: absolute;
-    top: -52px;
-    right: 260px;
+    top: -61px;
+    right: 213px;
     display: flex;
     align-items: center;
     justify-content: flex-start;
 
     > select {
     -webkit-appearance: none;
-    width: 200px;
-    background-color: transparent;
-    color: var(--color-grey300);
-    padding: 5px;
+    background-color: var(--color-primary);
+    color: var(--color-grey50);
+    padding: 13px;
     font-size: 14px;
     font-weight: 400;
     text-align: center;
     border-style: none;
     outline-style: none;
+    border-radius: 2px;
+    cursor: pointer;
     }
 `
