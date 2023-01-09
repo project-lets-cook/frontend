6d64b5c4d93677af160e72e6025d5c41e7deb3a6
davi <daavisouza.santana@gmail.com>
2023-01-09 16:19:36 -0300

    fix: fixing modal default

--- src/Components/Modal/styles.ts
+++ src/Components/Modal/styles.ts
@@ -17,4 +17,58 @@ export const StyledModal = styled.div`
     height: 100vh;
     background-color: var(--color-grey50);
   }
+
+  .default-modal-header {
+    display: flex;
+    justify-content: space-between;
+    width: 95%;
+    margin-top: 50px;
+    margin-bottom: 50px;
+  }
+
+  .default-modal-header > p {
+    font-size: 20px;
+    font-weight: 600;
+    color: var(--color-grey900);
+  }
+
+  .default-modal-header > button {
+    background-color: transparent;
+    border: none;
+  }
+
+  .form-input-box {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .form-input-box > form {
+    width: 95%;
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+    margin-bottom: 30px;
+  }
+
+  .form-input-box > form .input-warning {
+    margin-top: 5px;
+    color: var(--color-primary);
+  }
+
+  form p {
+    text-align: right;
+    margin-top: 5px;
+    font-size: 11px;
+    color: var(--color-error);
+  }
+
+  .modal-box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow-y: auto;
+    height: 100%;
+    padding: 0 30px;
+  }
 `;
