51316d2759b2a2284321100b2ea19df6793684cb
natabgaf <natanfernandes52@gmail.com>
2023-01-09 09:38:49 -0300

    fea/img basic

--- src/Components/CardDonor/Image/index.tsx
+++ src/Components/CardDonor/Image/index.tsx
@@ -5,6 +5,7 @@ import Vegetables from "../../../assets/icons/Vegetables.svg"
 import MeatAndEggs from "../../../assets/icons/MeatAndEggs.svg"
 import Milk from "../../../assets/icons/Milk.png"
 import oils from "../../../assets/icons/oils.svg"
+import basicbasket from "../../../assets/icons/basicbasket.svg"
 
 interface icategory {
   category: string;
@@ -14,12 +15,13 @@ export const ImageCards = ({ category }: icategory) => {
   
   return (
     <StyledImageCards>
-      {category === "Cereals" && < img src={Cereals} alt={category} />}
-      {category === "Vegetables" && <img src={Vegetables} alt={category} />}
-      {category === "Fruits" && <img src={Fruits} alt={category} />}
-      {category === "MeatAndEggs" && <img src={MeatAndEggs} alt={category} />}
-      {category === "Milk" && <img src={Milk} alt={category} />}
-      {category === "oils" && <img src={oils} alt={category} />}
+      {category === "Cereals" && < img src={Cereals} alt={"Cereais"} />}
+      {category === "Vegetables" && <img src={Vegetables} alt={"Hortaliças"} />}
+      {category === "Fruits" && <img src={Fruits} alt={"Frutas"} />}
+      {category === "MeatAndEggs" && <img src={MeatAndEggs} alt={"Carnes & Ovos"} />}
+      {category === "Milk" && <img src={Milk} alt={"Leite e Derivados"} />}
+      {category === "oils" && <img src={oils} alt={"Oleos & Gorduras"} />}
+      {category === "nonPerishable" && <img src={basicbasket} alt={"Não Pereciveis"} />}
     </StyledImageCards>
   )
 }
