15c45bfb2d15d4896aeab9d5b49a55354b54b3d4
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2023-01-10 13:33:57 -0300

    fix: card image error

--- src/Components/CardDonor/Image/index.tsx
+++ src/Components/CardDonor/Image/index.tsx
@@ -1,12 +1,12 @@
-import { StyledImageCards } from "./styles"
-import Cereals from "../../../assets/img/Cereals.jpeg"
-import Fruits from "../../../assets/img/fruits.jpg"
-import Vegetables from "../../../assets/img/Vegetables.jpg"
-import MeatAndEggs from "../../../assets/img/MeatAndEggs.jpg"
-import Milk from "../../../assets/img/Milk.jpg"
-import oils from "../../../assets/img/oils.jpg"
-import basicbasket from "../../../assets/img/basicbasket.jpg"
-import NoPherecives from "../../../assets/img/NoPherecives.jpg"
+import { StyledImageCards } from "./styles";
+import Cereals from "../../../assets/img/Cereals.jpeg";
+import Fruits from "../../../assets/img/fruits.jpg";
+import Vegetables from "../../../assets/img/Vegetables.jpg";
+import MeatAndEggs from "../../../assets/img/MeatAndEggs.jpg";
+import Milk from "../../../assets/img/Milk.jpg";
+import oils from "../../../assets/img/oils.jpg";
+import basicbasket from "../../../assets/img/basicbasket.jpg";
+import NoPherecives from "../../../assets/img/NoPherecives.jpg";
 
 interface icategory {
   category: string;
@@ -15,14 +15,24 @@ interface icategory {
 export const ImageCards = ({ category }: icategory) => {
   return (
     <StyledImageCards>
-      {category === "Cereais" && < img src={Cereals} alt={"Cereais"} />}
+      {category === "Cereais" && <img src={Cereals} alt={"Cereais"} />}
       {category === "Hortaliças" && <img src={Vegetables} alt={"Hortaliças"} />}
       {category === "Frutas" && <img src={Fruits} alt={"Frutas"} />}
-      {category === "Carnes & Ovos" && <img src={MeatAndEggs} alt={"Carnes & Ovos"} />}
-      {category === "Leite e Derivados" && <img src={Milk} alt={"Leite e Derivados"} />}
-      {category === "Oleos & Gorduras" && <img src={oils} alt={"Oleos & Gorduras"} />}
-      {category === "Cesta Basica" && <img src={basicbasket} alt={"Cesta Basica"} />}
-      {category === "Não Pereciveis" && <img src={NoPherecives} alt={"Não Pereciveis"} />}
+      {category === "Carnes & Ovos" && (
+        <img src={MeatAndEggs} alt={"Carnes & Ovos"} />
+      )}
+      {category === "Leite e Derivados" && (
+        <img src={Milk} alt={"Leite e Derivados"} />
+      )}
+      {category === "Óleos & Gorduras" && (
+        <img src={oils} alt={"Óleos & Gorduras"} />
+      )}
+      {category === "Cesta Basica" && (
+        <img src={basicbasket} alt={"Cesta Basica"} />
+      )}
+      {category === "Não Pereciveis" && (
+        <img src={NoPherecives} alt={"Não Pereciveis"} />
+      )}
     </StyledImageCards>
   );
 };
